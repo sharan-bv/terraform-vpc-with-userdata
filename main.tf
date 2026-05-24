a7509cf3f8d390c8397431bdec93c01c01eda5f2
Ashwini_naidu <ashwini.naidu@krishtech.dev>
2026-05-24 16:39:18 +0530

    changed the main file added the correct AZ

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidr
-  availability_zone = "ap-south-2a"
+  availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "private-subnet"
