7a5dc5c9cdf45a6c34b6e35853ae9256ee749874
Ashwini_naidu <ashwini.naidu@krishtech.dev>
2026-05-24 16:42:35 +0530

    changed the main file added the correct AZ

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
-  availability_zone       = "ap-south-2a"
+  availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
